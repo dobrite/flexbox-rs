ddc7415c2aa87995e8c16866a4f3ad3bd660bf06
David Obrite <david@ministrycentered.com>
2016-05-30 18:35:49 -0700

    chore(layout): derive Debug

--- src/layout.rs
+++ src/layout.rs
@@ -6,6 +6,7 @@ use super::rgb::RGB;
 
 // TODO wrap these or abtract them
 // TODO keep these pub or do some chaining builder thingy?
+#[derive(Debug)]
 pub struct Layout {
     pub bg: RGB<u8>,
     pub fg: RGB<u8>,
