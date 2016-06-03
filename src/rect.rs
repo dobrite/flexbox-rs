57b1793981dbe4bf181aba6c01a1decd8707959e
David Obrite <david@ministrycentered.com>
2016-06-03 09:53:11 -0700

    fix(rect): match sdl2 param order for Rect

--- src/rect.rs
+++ src/rect.rs
@@ -1,17 +1,17 @@
 
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Rect {
-    pub top: i32,
     pub left: i32,
+    pub top: i32,
     pub width: u32,
     pub height: u32,
 }
 
 impl Rect {
-    pub fn new(top: i32, left: i32, width: u32, height: u32) -> Self {
+    pub fn new(left: i32, top: i32, width: u32, height: u32) -> Self {
         Rect {
-            top: top,
             left: left,
+            top: top,
             width: width,
             height: height,
         }
