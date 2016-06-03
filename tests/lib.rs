941f016ecb76e26623bbbec86b8cd2beb2139621
David Obrite <david@ministrycentered.com>
2016-06-03 07:53:50 -0700

    style(tests): give names to indexed layout

--- tests/lib.rs
+++ tests/lib.rs
@@ -9,8 +9,9 @@ fn it_sets_root_width_to_width_with_default_height_no_children() {
     let height = 0u32;
     let root = Renderable::View(View::new(Style::new(), vec![]));
     let layout = layout(width, height, &root);
+    let root = &layout[0];
     assert!(layout.len() == 1);
-    assert_eq!(800, layout[0].rect.width);
+    assert_eq!(800, root.width());
 }
 
 #[test]
@@ -19,8 +20,9 @@ fn it_sets_root_height_to_0_with_default_height_no_children() {
     let height = 0u32;
     let root = Renderable::View(View::new(Style::new(), vec![]));
     let layout = layout(width, height, &root);
+    let root = &layout[0];
     assert!(layout.len() == 1);
-    assert_eq!(0, layout[0].rect.height);
+    assert_eq!(0, root.height());
 }
 
 #[test]
@@ -29,8 +31,9 @@ fn it_sets_root_bg_to_white_no_children() {
     let height = 600u32;
     let root = Renderable::View(View::new(Style::new(), vec![]));
     let layout = layout(width, height, &root);
+    let root = &layout[0];
     assert!(layout.len() == 1);
-    assert_eq!(RGB::new(255, 255, 255), layout[0].bg);
+    assert_eq!(RGB::new(255, 255, 255), root.bg());
 }
 
 #[test]
@@ -39,8 +42,9 @@ fn it_sets_root_fg_to_black_no_children() {
     let height = 600u32;
     let root = Renderable::View(View::new(Style::new(), vec![]));
     let layout = layout(width, height, &root);
+    let root = &layout[0];
     assert!(layout.len() == 1);
-    assert_eq!(RGB::new(0, 0, 0), layout[0].fg);
+    assert_eq!(RGB::new(0, 0, 0), root.fg());
 }
 
 #[test]
@@ -49,8 +53,9 @@ fn it_sets_root_width_to_800_with_no_children() {
     let height = 100u32;
     let root = Renderable::View(View::new(Style::new(), vec![]));
     let layout = layout(width, height, &root);
+    let root = &layout[0];
     assert!(layout.len() == 1);
-    assert_eq!(800, layout[0].rect.width);
+    assert_eq!(800, root.width());
 }
 
 #[test]
@@ -59,8 +64,9 @@ fn it_sets_root_height_to_100_with_no_children() {
     let height = 100u32;
     let root = Renderable::View(View::new(Style::new(), vec![]));
     let layout = layout(width, height, &root);
+    let root = &layout[0];
     assert!(layout.len() == 1);
-    assert_eq!(100, layout[0].rect.height);
+    assert_eq!(100, root.height());
 }
 
 #[test]
@@ -73,9 +79,10 @@ fn it_sets_child_dim_with_no_child_height() {
         Renderable::View(View::new(Style::new().with_width(child_width), vec![])),
     ]));
     let layout = layout(width, height, &root);
+    let child = &layout[1];
     assert!(layout.len() == 2);
-    assert_eq!(child_width, layout[1].rect.width);
-    assert_eq!(height, layout[1].rect.height);
+    assert_eq!(child_width, child.width());
+    assert_eq!(height, child.height());
 }
 
 #[test]
@@ -88,7 +95,8 @@ fn it_sets_child_dim_with_no_child_width() {
         Renderable::View(View::new(Style::new().with_height(child_height), vec![])),
     ]));
     let layout = layout(width, height, &root);
+    let child = &layout[1];
     assert!(layout.len() == 2);
-    assert_eq!(child_height, layout[1].rect.height);
-    assert_eq!(0, layout[1].rect.width);
+    assert_eq!(child_height, child.height());
+    assert_eq!(0, child.width());
 }
