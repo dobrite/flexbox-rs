c60e0563ce211842f006fb2c3afc67827cf28483
David Obrite <david@ministrycentered.com>
2016-06-04 08:45:42 -0700

    feat(ttf): showing font

--- examples/sdl2.rs
+++ examples/sdl2.rs
@@ -6,13 +6,8 @@ mod sdl2_utils;
 
 use std::path::Path;
 
-// TODO fg should determine this
-use sdl2::pixels::Color::RGBA;
-// TODO put in renderer
-use sdl2::render::TextureQuery;
-
 use sdl2_utils::Events;
-use flexbox::{layout, FlexDirection, Renderable, RGB, Style, View, Render};
+use flexbox::{layout, FlexDirection, Renderable, RGB, Style, View, Render, sdl2_backend};
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -28,7 +23,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut renderer = window.renderer()
+    let mut sdl2_renderer = window.renderer()
         .accelerated()
         .build()
         .unwrap();
@@ -43,11 +38,7 @@ fn main() {
         ])),
     ]));
 
-    let mut font = ttf_context.load_font(Path::new("./examples/assets/fonts/Monospace.ttf"), 12).unwrap();
-    let surface = font.render("Hello Rust!").blended(RGBA(255, 0, 0, 255)).unwrap();
-    let mut texture = renderer.create_texture_from_surface(&surface).unwrap();
-    let TextureQuery { width, height, .. } = texture.query();
-    renderer.copy(&mut texture, None, Some(Rect::new(0, 0, 200, 100)));
+    let mut renderer = sdl2_backend::Renderer::new(sdl2_renderer, ttf_context, Path::new("./examples/assets/fonts/Monospace.ttf"));
 
     loop {
         events.pump();
--- src/backend/sdl2_backend.rs
+++ src/backend/sdl2_backend.rs
@@ -1,20 +1,63 @@
 #![cfg(feature = "sdl2")]
 
 pub extern crate sdl2;
+pub extern crate sdl2_ttf;
+
+use std::path::Path;
 
 use layout::Layout;
 use render::Render;
 use rect::Rect;
 use rgb::RGB;
 
-impl<'a> Render for sdl2::render::Renderer<'a> {
+// TODO fg should determine font color
+
+pub struct Renderer<'a> {
+    renderer: sdl2::render::Renderer<'a>,
+    ttf_context: sdl2_ttf::Sdl2TtfContext,
+    font: sdl2_ttf::Font,
+}
+
+impl<'a> Renderer<'a> {
+    pub fn new(renderer: sdl2::render::Renderer<'a>,
+               ttf_context: sdl2_ttf::Sdl2TtfContext,
+               font_path: &Path)
+               -> Self {
+
+        let font = ttf_context.load_font(font_path, 24).unwrap();
+
+        Renderer {
+            renderer: renderer,
+            ttf_context: ttf_context,
+            font: font,
+        }
+    }
+}
+
+impl<'a> Render for Renderer<'a> {
     fn render(&mut self, layout: &[Layout]) {
+        self.renderer.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        self.renderer.clear();
+
         for l in layout {
-            self.set_draw_color(to_sdl2_color(l.bg));
-            let _ = self.fill_rect(to_sdl2_rect(l.rect));
+            self.renderer.set_draw_color(to_sdl2_color(l.bg));
+            let _ = self.renderer.fill_rect(to_sdl2_rect(l.rect));
         }
 
-        self.present();
+        let surface = self.font
+            .render("Hello Rust!")
+            .blended(sdl2::pixels::Color::RGBA(255, 0, 0, 255))
+            .expect("blah");
+
+        let mut texture = self.renderer.create_texture_from_surface(&surface).unwrap();
+
+        let sdl2::render::TextureQuery { width, height, .. } = texture.query();
+
+        self.renderer.copy(&mut texture,
+                           None,
+                           Some(sdl2::rect::Rect::new(100, 100, width, height)));
+
+        self.renderer.present();
     }
 }
 
